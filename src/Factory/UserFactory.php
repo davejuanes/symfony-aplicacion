19c880dd1f4969c0e0241ae4f6be4a5d9097cde7
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-08-07 12:53:50 -0400

    [CLASS 15] Encriptación de Contraseñas en Symfony

--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -5,18 +5,22 @@ namespace App\Factory;
 use App\Entity\User;
 use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 /**
  * @extends PersistentProxyObjectFactory<User>
  */
 final class UserFactory extends PersistentProxyObjectFactory
 {
+    private $hasher;
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
      *
      * @todo inject services if required
      */
-    public function __construct()
+    public function __construct(UserPasswordHasherInterface $hasher)
     {
+        $this->hasher = $hasher;
     }
 
     public static function class(): string
@@ -44,8 +48,11 @@ final class UserFactory extends PersistentProxyObjectFactory
      */
     protected function initialize(): static
     {
-        return $this
-            // ->afterInstantiate(function(User $user): void {})
+        return $this->afterInstantiate(function(User $user): void {
+            $user->setPassword(
+                $this->hasher->hashPassword($user, $user->getPassword())
+            );
+        })
         ;
     }
 }
